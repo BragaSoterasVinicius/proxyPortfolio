409ec703b422837f7680acc19607c739ae2638bf
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-04-17 21:23:46 -0300

    sistema de retorno de registros no banco funcionando

---
+++ js/loadPostLinks.js
@@ -0,0 +1,19 @@
+function loadPosts(){
+    fetch('http://localhost:3000/posts')
+      .then(response => response.json())
+      .then(data => {
+        const container = document.getElementById('linkDePostagens');
+
+        data.forEach(post => {
+          const div = document.createElement('div');
+          div.className = 'post';
+          div.innerHTML = `
+            <strong>Id:</strong> ${post.id}<br>
+            <strong>Titulo:</strong> ${post.Titulo}<br>
+            <strong>Date:</strong> ${post.date}<br>
+          `;
+          container.appendChild(div);
+        });
+      })
+      .catch(err => console.error('Error:', err));
+}
