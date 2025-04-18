fae266445cc7d4e51753061a5b58222d6830ef67
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-04-18 00:37:35 -0300

    sistema de recebimento de links de imagens finalizado

--- js/loadPostLinks.js
+++ js/loadPostLinks.js
@@ -1,3 +1,18 @@
+function loadImages(post_id, img_pos){
+  return fetch(`http://localhost:3000/linkimg/${post_id}/${img_pos}`)
+  .then(response => response.json())
+  .then(data => {
+      if (data.length > 0) {
+          return data[0].img_link; // Return the first image link
+      } else {
+          return 'Imagem indisponivel';
+      }
+  })
+  .catch(err => {
+      console.error('Error:', err);
+      return 'Erro de carregamento de imagem.';
+  });
+    }
 
 function loadPosts(){
     fetch('http://localhost:3000/posts')
@@ -10,10 +25,18 @@ function loadPosts(){
           div.className = 'post';
           div.innerHTML = `
             <strong>Id:</strong> ${post.id}<br>
+            <strong>Imagem</strong> <span id="image-${post.id}"><br>
             <strong>Titulo:</strong> ${post.Titulo}<br>
             <strong>Date:</strong> ${post.date}<br>
+
           `;
           container.appendChild(div);
+          loadImages(post.id, 0).then(link => {
+            const imageSpan = document.getElementById(`image-${post.id}`);
+            if (imageSpan) {
+                imageSpan.innerHTML = `<img src="${link}" alt="Post Image" />`;
+            }
+        });
         });
       })
       .catch(err => console.error('Error:', err));
@@ -30,10 +53,14 @@ function loadTags(){
           div.className = 'tagselector';
           div.innerHTML = `
             <strong>Tag:</strong> ${tag.tag_id}<br>
-            <strong>Titulo:</strong> ${tag.tag_id}<br>
+            <strong>Titulo:</strong> ${tag.tag_name}<br>
           `;
           container.appendChild(div);
         });
       })
       .catch(err => console.error('Error:', err));
 }
+
+
+loadPosts();
+loadTags();
