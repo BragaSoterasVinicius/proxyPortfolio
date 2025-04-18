8bb84e98480efe0c7ae770ac026d0afab03eec4e
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-04-17 21:33:37 -0300

    Chamada de tags e posts finalizada

--- js/loadPostLinks.js
+++ js/loadPostLinks.js
@@ -18,3 +18,22 @@ function loadPosts(){
       })
       .catch(err => console.error('Error:', err));
 }
+
+function loadTags(){
+    fetch('http://localhost:3000/tags')
+      .then(response => response.json())
+      .then(data => {
+        const container = document.getElementById('filtro');
+
+        data.forEach(tag => {
+          const div = document.createElement('div');
+          div.className = 'tagselector';
+          div.innerHTML = `
+            <strong>Tag:</strong> ${tag.tag_id}<br>
+            <strong>Titulo:</strong> ${tag.tag_id}<br>
+          `;
+          container.appendChild(div);
+        });
+      })
+      .catch(err => console.error('Error:', err));
+}
